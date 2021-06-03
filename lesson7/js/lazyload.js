204f368623505baa8158a47b3c7b2591fd97af2d
emlemmon <lem19012@byui.edu>
2021-06-03 16:08:20 -0600

    Lazy load activity and presentation html/css

---
+++ lesson7/js/lazyload.js
@@ -0,0 +1,43 @@
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+function preLoadImage(img) {
+    const src = img.getAttribute("data-src");
+    if (!src) {
+        return;
+    }
+    img.onload = () => {
+        img.removeAttribute('data-src');
+    };
+    img.src = src;
+}
+
+const imgOptions = {
+    threshold: 0.2,
+    rootMargin: "0px 0px -200px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        }
+        else {
+            preLoadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+imagesToLoad.forEach(image => {
+    imgObserver.observe(image);
+})
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+/*imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });*/
